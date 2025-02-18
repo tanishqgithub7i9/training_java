3c64bef62789e9c6ecc7a1f6dcaaa94940e4c172
tanishqsingh7i9 <tanishqdos@gmail.com>
2025-02-18 00:40:54 -0800

    add ques

---
+++ LengthExample.java
@@ -0,0 +1,9 @@
+public class LengthExample{
+    public static void main(String args[]){
+        String s1="java";
+        String s2="python";
+        System.out.println("String length is:"+s1.length());
+        System.out.println("String length is:"+s2.length());
+
+    }
+}

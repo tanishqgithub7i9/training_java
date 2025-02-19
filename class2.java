8ccc16395f95a58f42c68059930bf344668a0fe3
tanishqsingh7i9 <tanishqdos@gmail.com>
2025-02-19 01:22:21 -0800

    .gitignore file added

---
+++ class2.java
@@ -0,0 +1,12 @@
+class Student{
+    int id;
+    String name;
+}
+class class2{
+    public static void main(String []args){
+        Student s1=new Student();
+        s1.id=101;
+        s1.name="swet";
+        System.out.print(s1.id+" "+s1.name);
+    }
+}
